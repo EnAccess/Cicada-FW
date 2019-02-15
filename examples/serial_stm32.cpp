4345228336af2679a21c4da5237f50a9e6a948a0
Georg Lippitsch <georg@okrasolar.com>
2019-02-15 15:21:46 +0700

    Add serial example for STM32

---
+++ examples/serial_stm32.cpp
@@ -0,0 +1,137 @@
+/*
+ * Example code for serial communication on STM32
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "escheduler.h"
+#include "eserial.h"
+#include "etick.h"
+#include "stm32f1xx_hal.h"
+
+static void SystemClock_Config(void);
+
+class SerialTask : public ETask
+{
+public:
+    SerialTask(ESerial& serial) :
+        m_serial(serial),
+        m_i(0)
+    { }
+
+    virtual void run()
+    {
+    E_BEGIN_TASK
+
+        if (!m_serial.setSerialConfig(115200, 8))
+        {
+            printf("Error setting serial configuration\n");
+            exit(1);
+        }
+
+        if (!m_serial.open())
+        {
+            printf("Error opening serial port %s\n", m_serial.portName());
+            exit(1);
+        }
+
+        if (m_serial.portName())
+            printf("Serial port %s open\n", m_serial.portName());
+        else
+            printf("Serial port open\n");
+
+        for (m_i=0; m_i<100; m_i++)
+        {
+            {
+                const char* send_str = "AT\r\n";
+                printf("Sending command: %s", send_str);
+                int bytesWritten =
+                    m_serial.write(send_str, strlen(send_str));
+                printf("%d bytes written\n", bytesWritten);
+            }
+
+            E_REENTER_COND_DELAY(m_serial.bytesAvailable(), 100);
+
+            {
+                char buf[32];
+                int bytesReceived;
+                bytesReceived = m_serial.read(buf, 31);
+                printf("%d bytes received\n", bytesReceived);
+
+                buf[bytesReceived] = '\0';
+                printf("Received message: %s", buf);
+            }
+
+            E_REENTER_DELAY(500);
+        }
+
+        m_serial.close();
+
+        printf("Serial port closed\n");
+
+    E_END_TASK
+    }
+
+private:
+    EStm32Uart& m_serial;
+    int m_i;
+};
+
+int main(int argc, char * argv[])
+{
+    // System configuration
+    HAL_Init();
+    SystemClock_Config();
+
+    ESerial serial;
+    SerialTask task(serial);
+
+    ETask* taskList[] = {&task, dynamic_cast<ETask*>(&serial), NULL};
+
+    EScheduler s(&eTickFunction, taskList);
+    s.start();
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /**Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+}
+
+/* Interrupt handler */
+extern "C"
+{
+    void SysTick_Handler()
+    {
+        HAL_IncTick();
+    }
+
+    void USART3_IRQHandler()
+    {
+        EStm32Uart* instance = EStm32Uart::getInstance(USART3);
+        if (instance)
+            instance->handleInterrupt();
+    }
+}
