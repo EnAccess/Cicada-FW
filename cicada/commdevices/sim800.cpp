EnAccess/Cicada-FW

commit 79ab381624723e0cebab21124dbbbb3f050be1c0

SIM800: Handle more error cases

cicada/commdevices/sim800.cpp
@@ -56,9 +56,11 @@ void Sim800CommDevice::run()
         // Log the current modem states
         logStates(_sendState, _replyState);
 
-        // Handle deactivated or error state
+        // Handle deactivated or error states
         if (strncmp(_lineBuffer, "+PDP: DEACT", 11) == 0 ||
-            strncmp(_lineBuffer, "+CME ERROR", 10) == 0) {
+            strncmp(_lineBuffer, "+CME ERROR", 10) == 0 ||
+            strncmp(_lineBuffer, "ERROR", 5) == 0 ||
+            (_stateBooleans & RESET_PENDING)) {
             _serial.flushReceiveBuffers();
             _bytesToRead = 0;
             _bytesToReceive = 0;
@@ -72,7 +74,11 @@ void Sim800CommDevice::run()
         }
 
         // If sent a command, process standard reply
-        processStandardReply();
+        if (_waitForReply) {
+            if (strncmp(_lineBuffer, _waitForReply, strlen(_waitForReply)) == 0) {
+                _waitForReply = NULL;
+            }
+        }
 
         // Process replies which need special treatment
         switch (_replyState) {
@@ -93,6 +99,12 @@ void Sim800CommDevice::run()
         case cdnsgip:
             if (parseDnsReply()) {
                 _replyState = okReply;
+            }
+            break;
+
+        case cipstart:
+            if (strncmp(_lineBuffer, "0, CONNECT FAIL", 15) == 0) {
+                _stateBooleans |= RESET_PENDING;
             }
             break;
 
@@ -199,6 +211,7 @@ void Sim800CommDevice::run()
     case sendCipstart:
         SimCommDevice::sendCipstart("START");
 
+        _replyState = cipstart;
         _waitForReply = "0, CONNECT OK";
         _sendState = finalizeConnect;
         break;

cicada/commdevices/sim7x00.cpp
@@ -80,7 +80,16 @@ void Sim7x00CommDevice::run()
         logStates(_sendState, _replyState);
 
         // If sent a command, process standard reply
-        processStandardReply();
+        if (_waitForReply) {
+            if (strncmp(_lineBuffer, _waitForReply, strlen(_waitForReply)) == 0) {
+                _waitForReply = NULL;
+            } else if (strncmp(_lineBuffer, "ERROR", 5) == 0) {
+                _stateBooleans |= RESET_PENDING;
+                _connectState = generalError;
+                _waitForReply = NULL;
+                return;
+            }
+        }
 
         // Process replies which need special treatment
         switch (_replyState) {

cicada/commdevices/sim800.h
@@ -52,6 +52,7 @@ class Sim800CommDevice : public SimCommDevice
         okReply = 0,
         cifsr,
         cdnsgip,
+        cipstart,
         ciprxget4,
         ciprxget2
     };

cicada/commdevices/simcommdevice.cpp
@@ -128,21 +128,6 @@ void SimCommDevice::logStates(int8_t sendState, int8_t replyState)
     */
 }
 
-void SimCommDevice::processStandardReply()
-{
-    // Process standard reply, set by _waitForReply string
-    if (_waitForReply) {
-        if (strncmp(_lineBuffer, _waitForReply, strlen(_waitForReply)) == 0) {
-            _waitForReply = NULL;
-        } else if (strncmp(_lineBuffer, "ERROR", 5) == 0) {
-            _stateBooleans |= RESET_PENDING;
-            _connectState = generalError;
-            _waitForReply = NULL;
-            return;
-        }
-    }
-}
-
 bool SimCommDevice::parseDnsReply()
 {
     if (strncmp(_lineBuffer, "+CDNSGIP: 1", 11) == 0) {
@@ -173,6 +158,8 @@ bool SimCommDevice::parseDnsReply()
         }
         strcpy(_ip, tmpStr);
         return true;
+    } else if (strncmp(_lineBuffer, "+CDNSGIP: 0", 11) == 0) {
+        _stateBooleans |= RESET_PENDING;
     }
 
     return false;

cicada/commdevices/simcommdevice.h
@@ -85,7 +85,6 @@ class SimCommDevice : public IPCommDevice
   protected:
     bool fillLineBuffer();
     void logStates(int8_t sendState, int8_t replyState);
-    void processStandardReply();
     bool parseDnsReply();
     bool parseCiprxget4();
     bool parseCiprxget2();

commit 267513b71c09f633b15190c50650ce73ddc72eb8

Fix SIM800
It was broken by commit 79ab381624723e0cebab21124dbbbb3f050be1c0

cicada/commdevices/sim800.cpp
@@ -103,7 +103,9 @@ void Sim800CommDevice::run()
             break;
 
         case cipstart:
-            if (strncmp(_lineBuffer, "0, CONNECT FAIL", 15) == 0) {
+            if (_waitForReply == NULL) {
+                _replyState = okReply;
+            } else if (strncmp(_lineBuffer, "0, CONNECT FAIL", 15) == 0) {
                 _stateBooleans |= RESET_PENDING;
                 _connectState = generalError;
             }