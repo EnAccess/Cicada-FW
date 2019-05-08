EnAccess/Cicada-FW

commit c007171f3b255ac9031281835ab697dab5cec135

SIM7x00: Handle more error cases

cicada/commdevices/sim7x00.cpp
@@ -112,6 +112,15 @@ void Sim7x00CommDevice::run()
             }
             break;
 
+        case cipopen:
+            if (_waitForReply != NULL) {
+                if (strncmp(_lineBuffer, "+CIPOPEN: 0,", 12) == 0) {
+                    _stateBooleans |= RESET_PENDING;
+                    _connectState = generalError;
+                }
+            }
+            break;
+
         case ciprxget4:
             if (parseCiprxget4()) {
                 _replyState = okReply;
@@ -232,6 +241,7 @@ void Sim7x00CommDevice::run()
     case sendCipopen: {
         SimCommDevice::sendCipstart("OPEN");
 
+        _replyState = cipopen;
         _waitForReply = "+CIPOPEN: 0,0";
         _sendState = finalizeConnect;
         break;

cicada/commdevices/sim7x00.h
@@ -48,7 +48,15 @@ class Sim7x00CommDevice : public SimCommDevice
     virtual void run();
 
   private:
-    enum ReplyState { okReply = 0, expectConnect, netopen, cdnsgip, ciprxget4, ciprxget2 };
+    enum ReplyState {
+        okReply = 0,
+        expectConnect,
+        netopen,
+        cdnsgip,
+        cipopen,
+        ciprxget4,
+        ciprxget2
+    };
 
     enum SendState {
         notConnected,

cicada/commdevices/sim800.cpp
@@ -105,6 +105,7 @@ void Sim800CommDevice::run()
         case cipstart:
             if (strncmp(_lineBuffer, "0, CONNECT FAIL", 15) == 0) {
                 _stateBooleans |= RESET_PENDING;
+                _connectState = generalError;
             }
             break;
 

commit 178d74d2c6d46318ba24733effe431bcc7a7067a

Fix SIM7x00
It was broken by commit c007171f3b255ac9031281835ab697dab5cec135

cicada/commdevices/sim7x00.cpp
@@ -113,7 +113,9 @@ void Sim7x00CommDevice::run()
             break;
 
         case cipopen:
-            if (_waitForReply != NULL) {
+            if (_waitForReply == NULL) {
+                _replyState = okReply;
+            } else {
                 if (strncmp(_lineBuffer, "+CIPOPEN: 0,", 12) == 0) {
                     _stateBooleans |= RESET_PENDING;
                     _connectState = generalError;