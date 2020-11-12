c7d7a3bc5e4593e67c9e02b6e456712df55bb65c
Georg Lippitsch <georg@okrasolar.com>
2020-11-12 13:47:52 +0100

    Fix handling read data register overflows
    If the receive register overflows, the ORE flag instead of the RXNE flag
    is set. We still need to read the DR register to clear the ORE flag,
    otherwise the interrupt will be retriggered immediately. See USART_SR
    (27.6.1) in the STM32F103xx reference manual (RM0008) for details.

--- cicada/platform/stm32f1/stm32uart.cpp
+++ cicada/platform/stm32f1/stm32uart.cpp
@@ -216,7 +216,8 @@ const char* Stm32Uart::portName() const
 
 bool Stm32Uart::rawRead(uint8_t& data)
 {
-    if (__HAL_UART_GET_FLAG(&_handle, UART_FLAG_RXNE)) {
+    if (__HAL_UART_GET_FLAG(&_handle, UART_FLAG_RXNE) ||
+        __HAL_UART_GET_FLAG(&_handle, UART_FLAG_ORE)) {
         data = (uint8_t)READ_REG(_handle.Instance->DR);
         return true;
     }
