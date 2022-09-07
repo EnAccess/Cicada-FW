c4a0f64858a92b9f25e18a8a56ec43e70eb2080b
Georg Lippitsch <georg@okrasolar.com>
2022-09-07 17:27:58 +0100

    SIM800: Use constant delay of 10ms between runs

--- cicada/commdevices/sim800.cpp
+++ cicada/commdevices/sim800.cpp
@@ -70,7 +70,6 @@ void Sim800CommDevice::run()
         _waitForReply = NULL;
         _stateBooleans &= ~RESET_PENDING;
         if (_connectState >= intermediate) {
-            setDelay(2000);
             connect();
         }
     }
@@ -202,7 +201,6 @@ void Sim800CommDevice::run()
         break;
 
     case connecting:
-        setDelay(10);
         _connectState = IPCommDevice::intermediate;
         _stateBooleans |= LINE_READ;
         _waitForReply = _okStr;
@@ -282,7 +280,6 @@ void Sim800CommDevice::run()
     }
 
     case finalizeConnect:
-        setDelay(0);
         _connectState = IPCommDevice::connected;
         _replyState = okReply;
         _sendState = connected;
@@ -315,7 +312,6 @@ void Sim800CommDevice::run()
         break;
 
     case sendCiprxget4:
-        setDelay(0);
         _waitForReply = _okStr;
         _sendState = sendCiprxget2;
         _replyState = ciprxget4;
@@ -351,7 +347,6 @@ void Sim800CommDevice::run()
         } else if (_bytesToReceive > 0) {
             _sendState = sendCiprxget2;
         } else {
-            setDelay(10);
             _sendState = sendCiprxget4;
         }
         break;
