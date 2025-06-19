8a63795a12769107530c3f385136a449181cc7bb
Georg Lippitsch <georg.lippitsch@gmx.at>
2025-06-19 22:03:00 +0200

    CC1352P7: Treat SEND FAIL as disconnect, not error

--- cicada/commdevices/cc1352p7.cpp
+++ cicada/commdevices/cc1352p7.cpp
@@ -156,11 +156,10 @@ void CC1352P7CommDevice::run()
     }
 
     // If a modem reset is pending, handle it
-    if (_stateBooleans & RESET_PENDING) {
+    if (_stateBooleans & RESET_PENDING && _bytesToWrite == 0) {
         _serial.flushReceiveBuffers();
         _bytesToRead = 0;
         _bytesToReceive = 0;
-        _bytesToWrite = 0;
         _sendState = sendCipclose;
         _replyState = okReply;
         _waitForReply = NULL;
@@ -177,8 +176,7 @@ void CC1352P7CommDevice::run()
         logStates(_sendState, _replyState);
 
         // Handle error states
-        if (strncmp(_lineBuffer, "ERROR", 5) == 0
-            || (_sendState >= connected && strncmp(_lineBuffer, "SEND FAIL", 9) == 0)) {
+        if (strncmp(_lineBuffer, "ERROR", 5) == 0 ) {
             _stateBooleans |= RESET_PENDING;
             _connectState = generalError;
             _waitForReply = NULL;
@@ -227,7 +225,8 @@ void CC1352P7CommDevice::run()
             if (strncmp(_lineBuffer, "+IPD,", 4) == 0) {
                 _bytesToReceive = strtol(_lineBuffer + 5, NULL, 10);
                 _stateBooleans |= DATA_PENDING;
-            } else if (strncmp(_lineBuffer, "CLOSED", 6) == 0) {
+            } else if (strncmp(_lineBuffer, "CLOSED", 6) == 0 ||
+                       strncmp(_lineBuffer, "SEND FAIL", 9) == 0) {
                 _stateBooleans &= ~IP_CONNECTED;
             }
         }
